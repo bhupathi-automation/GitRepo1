bd66c1d132fa293f699c03cd472905e28fd2b716
Bhupathi <bhupathiraj253@gmail.com>
2021-08-13 14:58:43 +0530

    Editing File-GitLocal

--- src/test/java/testcases/general/PracticeTest.java
+++ src/test/java/testcases/general/PracticeTest.java
@@ -5,5 +5,7 @@ public class PracticeTest {
     public static void main(String[] args) {
         System.out.println("This is test class");
         System.out.println("ADDED NEW Line: This is test class");
+
+        System.out.println("Print #1 - GitLocal");
     }
 }
